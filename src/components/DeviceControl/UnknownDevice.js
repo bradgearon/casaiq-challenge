d33070d01c465870d4c6224029f2e971c6f52c44
Brad Gearon <bradgearon@gmail.com>
2017-12-04 05:21:13 -0600

    fixed typo in UnknownDevice

--- src/components/DeviceControl/UnknownDevice.js
+++ src/components/DeviceControl/UnknownDevice.js
@@ -4,10 +4,10 @@ import { Text } from '../common';
 
 import containerStyles from '../../styles/containers';
 
-const UnkownDevice = ({ device, style }) => (
+const UnknownDevice = ({ device, style }) => (
   <View style={[containerStyles.mm, style]}>
     <Text>We are not supporting this device at the moment</Text>
   </View>
 );
 
-export default UnkownDevice;
+export default UnknownDevice;
