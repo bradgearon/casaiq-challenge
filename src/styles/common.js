3ba74bcf23c0fc44cfd593e79feb2b41c3736fa5
Brad Gearon <bradgearon@gmail.com>
2017-12-04 03:19:16 -0600

    made default border radius 10

--- src/styles/common.js
+++ src/styles/common.js
@@ -5,7 +5,7 @@ export const COLOR_TEXT_SECONDARY = '#757575';
 export const COLOR_ACCENT = '#03A9F4';
 export const COLOR_ICON = '#757575';
 
-export const BORDER_RADIUS = 5;
+export const BORDER_RADIUS = 10;
 export const PADDING_DEFAULT = 10;
 export const ELEVATION_DEFAULT = 1.5;
 export const MARGIN_DEFAULT = 10;
